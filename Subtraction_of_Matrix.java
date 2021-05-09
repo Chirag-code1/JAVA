6fa3e64926785bf42d362f98a38a267eb4ba2de8
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-09 23:34:02 +0530

    Add files via upload

---
+++ Subtraction_of_Matrix.java
@@ -0,0 +1,40 @@
+public class Main  
+{  
+    public static void main(String[] args) {  
+        int rows, cols;  
+  
+         
+          int a[][] = {  
+                          {4, 5, 6},  
+                          {3, 4, 1},  
+                          {1, 2, 3}  
+                       };  
+  
+          
+          int b[][] = {  
+                          {2, 0, 3},  
+                         {2, 3, 1},  
+                         {1, 1, 1}  
+                     };  
+  
+          //Calculates number of rows and columns present in given matrix  
+          rows = a.length;  
+        cols = a[0].length;  
+  
+        int diff[][] = new int[rows][cols];  
+  
+        for(int i = 0; i < rows; i++){  
+            for(int j = 0; j < cols; j++){  
+                diff[i][j] = a[i][j] - b[i][j];  
+            }  
+        }  
+  
+        System.out.println("Subtraction of two matrices: ");  
+        for(int i = 0; i < rows; i++){  
+            for(int j = 0; j < cols; j++){  
+               System.out.print(diff[i][j] + " ");  
+            }  
+            System.out.println();  
+        }  
+    }  
+} 
