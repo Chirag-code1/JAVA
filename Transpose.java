d757fcbc2e0a06986f7a6eff678ae06010e34bef
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-27 23:47:04 +0530

    Update Transpose.java

--- Transpose.java
+++ Transpose.java
@@ -19,6 +19,7 @@ System.out.print(original[i][j]+" ");
 }    
 System.out.println();   
 }    
+    
 System.out.println("Printing Matrix After Transpose:");  
 for(int i=0;i<3;i++){    
 for(int j=0;j<3;j++){    
